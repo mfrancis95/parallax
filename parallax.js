774e689af861f4e287b55359d5e5498a95b8a2f8
mfrancis95 <mikefrancis95@gmail.com>
2016-04-23 12:36:11 -0400

    Extract background-position-x from background-position for Firefox

--- parallax.js
+++ parallax.js
@@ -1,12 +1,13 @@
 Element.prototype.parallaxify = function() {
     var self = this;
     var factor = self.dataset.factor || -1;
-    var xPos = window.getComputedStyle(element).getPropertyValue("background-position-x");
+    var xPos = window.getComputedStyle(self).getPropertyValue("background-position").split(" ")[0];
     var updating;
     var update = function() {
         self.style.backgroundPosition = xPos + " " + (window.scrollY * factor) + "px";
         updating = false;
     };
+    self.style.backgroundAttachment = "fixed";
     updating = true;
     update();
     window.addEventListener("scroll", function() {
