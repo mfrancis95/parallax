436bc7b4f9aa0dde5d5ac161e7d62c91ec6e49a5
mfrancis95 <mikefrancis95@gmail.com>
2016-05-03 22:17:30 -0400

    Add data-background attribute

--- parallax.js
+++ parallax.js
@@ -1,13 +1,22 @@
 Element.prototype.parallaxify = function() {
     var self = this;
-    var factor = self.dataset.factor || -1;
-    var xPos = window.getComputedStyle(self).getPropertyValue("background-position").split(" ")[0];
+    var dataset = self.dataset;
+    var factor = dataset.factor || -1;
     var updating;
-    var update = function() {
-        self.style.backgroundPosition = xPos + " " + (window.scrollY * factor) + "px";
-        updating = false;
-    };
-    self.style.backgroundAttachment = "fixed";
+    if (dataset.background) {
+        self.style.backgroundAttachment = "fixed";
+        var xPos = window.getComputedStyle(self).getPropertyValue("background-position").split(" ")[0];
+        var update = function() {
+            self.style.backgroundPosition = xPos + " " + (window.scrollY * factor) + "px";
+            updating = false;
+        };
+    }
+    else {
+        var update = function() {
+            self.style.transform = "translateY(" + (window.scrollY * factor) + "px)";
+            updating = false;
+        };
+    }
     updating = true;
     update();
     window.addEventListener("scroll", function() {
